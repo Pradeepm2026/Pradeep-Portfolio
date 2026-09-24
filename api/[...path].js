cf2ee2f3fb1a9c85416d776e86b8270432b73973
Pradeep Maurya <pradeepmauryasln2004@gmail.com>
2026-09-24 20:24:25 +0530

    Add Vercel API route

---
+++ api/[...path].js
@@ -0,0 +1,3 @@
+// Vercel serves this catch-all function for every /api/* request.
+// The Express application keeps the same routes locally and in production.
+module.exports = require('../server');
